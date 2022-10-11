4d14aed6247d70722dc7ad59b8152d28157a59cb
emma2625 <emmanuelodobo10@gmail.com>
2022-10-11 13:40:04 +0100

    First Commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comrade Music</title>
+    <link rel="stylesheet" href="assets/css/bootstrap5.min.css">
+    <link rel="stylesheet" href="assets/lib/fontawsome/css/all.css">
+
+    <link rel="shortcut icon" href="assets/img/logo.jpg">
+</head>
+<body>
+    <section>
+    <nav class="navbar bg-dark navbar-dark">
+        <div class="container">
+            <a class="navbar-brand d-flex gap-2 align-items-center" href="#">
+                <img src="assets/img/logo.png" alt="Bootstrap" height="74">
+                <p class="fw-bold fs-2">Comrade Music</p>
+            </a>
+        </div>
+    </nav>
+    </section>
+</body>
+</html>
